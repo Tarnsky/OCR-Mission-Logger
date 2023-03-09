2e845ada4907dd642c157bde836c682928322336
Ted Krimmer <ted.krimmer@gmail.com>
2023-03-09 12:45:06 +1100

    Failing to detect

--- app/src/main/java/com/example/remusocrlogger/MainActivity.kt
+++ app/src/main/java/com/example/remusocrlogger/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.remusocrlogger
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
@@ -13,120 +18,107 @@ import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.example.remusocrlogger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-
     lateinit var binding: ActivityMainBinding
-
-    private val REQUEST_IMAGE_CAPTURE=1
-
-    private var imageBitmap: Bitmap? =null
+    private var imageBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.apply {
-
             captureImage.setOnClickListener {
-
-                takeImage()
-
+                selectImage()
                 textView.text = ""
-
-
             }
-
             detectTextImageBtn.setOnClickListener {
-
                 processImage()
-
             }
-
+            buttonCopy.setOnClickListener {
+                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText("OCR Text", textView.text.toString())
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(this@MainActivity, "Text copied to clipboard", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
-
-
     }
 
-
-    private fun takeImage(){
-
-        val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-        try {
-
-            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE)
-
-        }
-        catch (e:Exception){
-
-
-
-        }
-
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode==REQUEST_IMAGE_CAPTURE && resultCode== RESULT_OK){
-
+    private val cameraActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data
             val extras: Bundle? = data?.extras
-
-            imageBitmap= extras?.get("data") as Bitmap
-
-            if (imageBitmap!=null) {
-
+            imageBitmap = extras?.get("data") as Bitmap
+            if (imageBitmap != null) {
                 binding.imageView.setImageBitmap(imageBitmap)
-
             }
-
-
-
         }
-
-
     }
 
-
-
-
-    private fun processImage(){
-
-        if (imageBitmap!=null) {
-
-            val image = imageBitmap?.let {
-
-                InputImage.fromBitmap(it, 0)
-
+    private val galleryActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data
+            val uri: Uri? = data?.data
+            try {
+                uri?.let { imageUri ->
+                    imageBitmap = MediaStore.Images.Media.getBitmap(
+                        contentResolver,
+                        imageUri
+                    )
+                    binding.imageView.setImageBitmap(imageBitmap)
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
 
+    private fun selectImage() {
+        val items = arrayOf<CharSequence>("Take photo", "Choose from gallery", "Cancel")
+        val builder = android.app.AlertDialog.Builder(this)
+        builder.setTitle("Add photo")
+        builder.setItems(items) { dialog, item ->
+            when {
+                items[item] == "Take photo" -> {
+                    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                        takePictureIntent.resolveActivity(packageManager)?.also {
+                            cameraActivityResultLauncher.launch(takePictureIntent)
+                        }
+                    }
+                }
+                items[item] == "Choose from gallery" -> {
+                    Intent(Intent.ACTION_PICK).also { galleryIntent ->
+                        galleryIntent.type = "image/*"
+                        galleryActivityResultLauncher.launch(galleryIntent)
+                    }
+                }
+                items[item] == "Cancel" -> {
+                    dialog.dismiss()
+                }
+            }
+        }
+        builder.show()
+    }
+
+    private fun processImage() {
+        if (imageBitmap != null) {
+            val image = imageBitmap?.let {
+                InputImage.fromBitmap(it, 0)
+            }
             image?.let {
                 recognizer.process(it)
                     .addOnSuccessListener { visionText ->
-
-                        binding.textView.text = visionText.text
-
+                        binding.textView.setText(visionText.text)
                     }
                     .addOnFailureListener { e ->
-
+                        Toast.makeText(this, "Failed to process image", Toast.LENGTH_SHORT).show()
                     }
             }
-
-        }
-
-        else{
-
+        } else {
             Toast.makeText(this, "Please select photo", Toast.LENGTH_SHORT).show()
-
         }
-
-
     }
-
-
-
 }
